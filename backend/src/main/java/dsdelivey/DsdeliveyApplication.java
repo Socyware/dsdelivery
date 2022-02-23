c286442264f84d20694d720fe35579fb4a6e0aed
Alan Souza <socyware@gmail.com>
2022-02-23 08:31:39 -0300

    Project created

---
+++ backend/src/main/java/dsdelivey/DsdeliveyApplication.java
@@ -0,0 +1,13 @@
+package dsdelivey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdeliveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdeliveyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dsdelivey/DsdeliveyApplicationTests.java
@@ -0,0 +1,13 @@
+package dsdelivey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
